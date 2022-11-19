360019417b9e351fd869b8cc19d8ce92a8c61e71
Allan Wang <allan8898@gmail.com>
2022-11-19 17:42:58 -0500

    Remove boilerplate

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo"]})
+    res.json({"users": ["userOne", "userTwo", "userFour"]})
 });
 
 app.listen(1025, () => {console.log("Server started on port 1025")})
